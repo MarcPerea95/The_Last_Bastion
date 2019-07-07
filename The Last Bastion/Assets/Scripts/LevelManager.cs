836f99792389caa0dbd9c55987d76f604c75465a
Marc Perea i Agustí <MarcPerea95@users.noreply.github.com>
2019-07-07 16:10:57 +0200

    Placing Tiles + Read Level from a .txt

--- The Last Bastion/Assets/Scripts/LevelManager.cs
+++ The Last Bastion/Assets/Scripts/LevelManager.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    /// <summary>
+    /// A prefab for creating a single tile
+    /// </summary>
+
+
     [SerializeField]
-    private GameObject tile;
+    private GameObject[] tilePrefabs;
+
+    public float TileSize
+    {
+        get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +30,44 @@ public class LevelManager : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Creates the level
+    /// </summary>
     private void CreateLevel()
     {
+        string[] mapData = ReadLevelText();
 
-        float tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        int mapX = mapData[0].ToCharArray().Length;
+        int mapY = mapData.Length;
 
-        for (int y = 0; y < 5; y++)
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+
+        for (int y = 0; y < mapY; y++)
         {
-            for (int x = 0; x < 5; x++)
+            char[] newTiles = mapData[y].ToCharArray();
+
+            for (int x = 0; x < mapX; x++)
             {
-                GameObject newTile = Instantiate(tile);
-                newTile.transform.position = new Vector3(tileSize * x, tileSize * y);
+                PlaceTile(newTiles[x].ToString(),x,y, worldStart);
             }
         }
+    }
+
+    private void PlaceTile(string TileType, int x, int y, Vector3 worldStart)
+    {
+        int tileIndex = int.Parse(TileType);
+
+        GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
+        newTile.transform.position = new Vector3(worldStart.x + TileSize* x, worldStart.y - TileSize * y, 0);
+    }
+
+
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Level") as TextAsset;
+
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+        return data.Split('-');
     }
 }
